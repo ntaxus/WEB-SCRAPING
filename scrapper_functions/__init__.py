69045bab4d4b700633d4590b955bd141bba7f62d
Antonio Gutiérrez <antonioguti576@gmail.com>
2021-11-01 14:13:29 +0100

    Inicializamos User-Agent que queremos

--- scrapper_functions/__init__.py
+++ scrapper_functions/__init__.py
@@ -1,6 +1,8 @@
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
+
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
 
 def setup_driver():
@@ -9,5 +11,13 @@ def setup_driver():
 
     :return: driver
     '''
+
+    # Inicializamos User-Agent que queremos
+    opts = Options()
+    opts.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+                      "AppleWebKit/537.36 (KHTML, like Gecko)"
+                      "Chrome/95.0.4638.54 Safari/537.36")
+
+    # Setup del webdriver
     s = Service(ChromeDriverManager().install())
-    return webdriver.Chrome(s.path)
+    return webdriver.Chrome(s.path, chrome_options=opts)
